ee393109ba21a77a7a6263c26106862ca10f5b01
dconnorj <connor_dalley@protonmail.com>
2024-09-15 17:19:06 -0600

    debugging chars

--- A1/decipher.py
+++ A1/decipher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+
 ciphertext = sys.argv[1]
 
 # Dictionary of English-language letter frequencies.
@@ -9,72 +10,79 @@ f = {"A": .08167, "B": .01492, "C": .02782, "D": .04253, "E": .12702, "F": .0222
      "S": .06327, "T": .09056, "U": .02758, "V": .00978, "W": .02360, "X": .00150,
      "Y": .01974, "Z": .00074}
 
+
 # Returns index for a given letter.
 def index(letter):
     return sorted(list(f.keys())).index(letter)
 
+
 # Returns letter for a given index.
 def letter(index):
-    if index > 25: index = index-26
+    if index > 25: index = index - 26
     return sorted(list(f.keys()))[index]
 
+
 def chi_squared(observed_count, expected):
-        return (**2(observed_count-expected))/expected
+    return ((observed_count - expected) ** 2) / expected
 
 
 def chi_score(column):
-    letter_occurances = {}
-    
+    letter_occurances = []
+
     for i in range(26):
         letter_occurances.append(0)
     for letter in column:
-        letter_occurances[letter-65] = letter_occurances[letter-65]+1
+        letter_occurances[ord(letter) - 65] = letter_occurances[ord(letter) - 65] + 1
     chi_score = 0
-    for i in range(26)
-        if(letter_occurences[i] != 0):
-             expected = len(column)*f[i]
-             chi_score += chi_squared(letter_occurences[i], expected)
+    for i in range(26):
+        if letter_occurances[i] != 0:
+            expected = len(column) * f[i]
+            chi_score += chi_squared(letter_occurances[i], expected)
     return chi_score
 
-def shifted_column(column, shift)
+
+def shifted_column(column, shift):
     shifted_column_output = ""
-    for letter in column:
-        shifted_column_output = shifted_column_output + (letter-shift)
-    reuturn shifted_column_output
+    col = list(column)
+    for letter in col:
+          shifted_column_output += chr(ord(letter)-shift)
+    return shifted_column_output
 
-def find_lowest_chi_score(column):
-    for i in range(26):
 
-    #uses shifted_column, find_observed_count and chi_squared
+def find_lowest_shift(column):
+    col = ''
+    min_chi = 9999999999
+    min_shift = 0
+    for shift in range(26):
+        col = shifted_column(column, shift)
+        chi = chi_score(col)
+        if chi < min_chi:
+            min_chi = chi
+            min_shift = shift
+    return min_shift
+
+    # uses shifted_column, find_observed_count and chi_squared
 
 
 # -----------------------------------------------------------------
 
-cipherLength = int(len(ciphertext)/8)
+cipherLength = int(len(ciphertext) / 8)
 block_list = []
 for x in range(cipherLength):
-        x=x*8
-        j = ciphertext[x:x+8]
-        block_list.append(j)
-        print(j)
-
-
-print("=============")
-print(block_list)
+    x = x * 8
+    j = ciphertext[x:x + 8]
+    block_list.append(j)
 
 column_strings = []
 for x in range(8):
-        column_strings.append("")
+    column_strings.append("")
 for block in block_list:
-        for character in range(8):
-                column_strings[character] += block[character]
-
-for column in 8:
-        minimun_chi
-        for shift in range(26):
-                chi_score()
-
+    for character in range(8):
+        column_strings[character] += block[character]
+key = ''
+for column in column_strings:
+    shift = find_lowest_shift(column)
+    key += letter(shift)
 
 # -----------------------------------------------------------------
-key = ""
 print(key)
