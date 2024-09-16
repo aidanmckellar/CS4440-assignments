2efb51b3cb192f29e51c1e1d45369b283961236e
cs4440 <aidan.mckellar7@gmail.com>
2024-09-16 10:35:26 -0600

    Started part 4 of project 1. Created bleichenbacher.py with default code

---
+++ A1/bleichenbacher.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from pyroots import *
+import hashlib
+import sys
+message = sys.argv[1]
+
+#----------------------------------------------
+# TODO: Your signature forgery code goes here!
+#----------------------------------------------
+
+forged_sig = ""
+print(integer_to_base64(forged_sig))
