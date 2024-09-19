71d26b3856424ed5802003ec5342eefc51a21aaa
cs4440 <aidan.mckellar7@gmail.com>
2024-09-19 16:31:43 -0600

    Fixed extend.py and cleaned up extend.py

--- A1/bleichenbacher.py
+++ A1/bleichenbacher.py
@@ -5,13 +5,18 @@ import sys
 import base64
 message = sys.argv[1]
 
-e= 3
+# Exponent hard coded to 3
+e = 3
+
+# Convert message to an int
 m = hashlib.sha1()
 m.update(message.encode())
 m.hexdigest()
-
 m = "0001ff003021300906052b0e03021a05000414" + m.hexdigest() + "00"*217 
 m_int = int(m, 16)
 
+# Take root of message integer value (m_int)
 cubed_root = integer_nthroot(m_int, e)
+
+# Add 1 to root value
 print(integer_to_base64(cubed_root[0] + 1))
--- A1/extend.py
+++ A1/extend.py
@@ -5,23 +5,21 @@ import sys, re
 from pymd5 import *
 url = sys.argv[1]
 
-split_url = url.split('token=')
-token_commands = split_url[1].split('&user=')
-num_commands =len(token_commands[1].split('&'))
 
-m ='12345678user=' + token_commands[1]
-print(m)
-padded_m_len =(len(m)  + len(padding((len(m)) * 8)))8
+m ='12345678' +  'user=admin&command1=ListFiles&command2=NoOp'
+padded_m_len =(len(m)  + len(padding((len(m)) * 8)))*8
 h2 = md5(
-        state = token_commands[0],
+        state = "402a574d265dc212ee64970f159575d0",
         count = padded_m_len
 
     )
 command  = '&command3=UnlockAllSafes'
 h2.update(command)
 parsedUrl = urlparse(url)
-
-q = 'token='+h2.hexdigest()+'&user='+ token_commands[1] + quote(padding(len(m)8))+'&command' +str(num_commands)+ '=UnlockAllSafes'
+q = 'token='+h2.hexdigest()+'&user=admin&command1=ListFiles&command2=NoOp'+ quote(padding(len(m)*8))+'&command3=UnlockAllSafes'
+z = len(padding(len(m)) * 8)
+y = len(m)
+l = "&command#=UnlockAllSafes"
 conn = httplib.HTTPConnection(parsedUrl.hostname,parsedUrl.port)
 conn.request("GET", parsedUrl.path + "?" + q)
 print(conn.getresponse().read())
