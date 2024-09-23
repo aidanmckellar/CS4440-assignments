62aae84b997c95554fa4bfc19cd556a251bc62cd
cs4440 <aidan.mckellar7@gmail.com>
2024-09-22 20:37:34 -0600

    Added some code to sol0.py and made some progress

--- A2/targets/sol0.py
+++ A2/targets/sol0.py
@@ -3,7 +3,8 @@ from struct import pack
 from shellcode import shellcode
 
 # Implement your attack here!
-payload = ''
+payload = b'u1191447  A+'
 
 # Launch the attack!
 sys.stdout.buffer.write(payload)
+#print(payload)
